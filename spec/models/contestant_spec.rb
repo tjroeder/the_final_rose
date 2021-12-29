dec8d4144bca7a336b5965fdf043ce7913624476
Tim Roeder <timothyjroeder@gmail.com>
2021-12-29 12:46:14 -0700

    Test: create relationship and validation tests

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Contestant, type: :model do
   describe 'relationships' do
-    it {should belong_to :bachelorette}
+    it { should belong_to :bachelorette }
+    it { should have_many :contestant_outings }
+    it { should have_many(:outings).through(:contestant_outings) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :age }
+    it { should validate_presence_of :hometown }
   end
 end
