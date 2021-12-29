d2ee70ae437b38a46d1ef076ad89f141eaa71fe3
Tim Roeder <timothyjroeder@gmail.com>
2021-12-29 12:44:58 -0700

    Test: create validation tests

--- spec/models/bachelorette_spec.rb
+++ spec/models/bachelorette_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Bachelorette, type: :model do
   describe 'relationships' do
-    it {should have_many :contestants}
+    it { should have_many :contestants }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :season_number }
+    it { should validate_presence_of :description }
   end
 end
