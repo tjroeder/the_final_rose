6dadbb484f2a5d0d1958e3daac6294caa8c66723
Tim Roeder <timothyjroeder@gmail.com>
2021-12-31 23:23:56 -0700

    Feat: #contestand_count method

--- app/models/outing.rb
+++ app/models/outing.rb
@@ -3,4 +3,9 @@ class Outing < ApplicationRecord
   has_many :contestants, through: :contestant_outings
 
   validates_presence_of :name, :location, :date
+
+  # Instance Methods
+  def contestant_count
+    contestants.count
+  end
 end
