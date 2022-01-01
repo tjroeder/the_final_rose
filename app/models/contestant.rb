d4afbd10127f7e61998b95ec501bcc30dae227bd
Tim Roeder <timothyjroeder@gmail.com>
2021-12-31 22:31:39 -0700

    Feat: add #season_number and #description methods

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -4,4 +4,13 @@ class Contestant < ApplicationRecord
   has_many :outings, through: :contestant_outings
 
   validates_presence_of :name, :age, :hometown
+
+  # Instance Methods
+  def season_number
+    bachelorette.season_number
+  end
+
+  def description
+    bachelorette.description
+  end
 end
