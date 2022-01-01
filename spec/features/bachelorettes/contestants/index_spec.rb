203ad057a584526b9d477b4dc1a7ac2ace30c885
Tim Roeder <timothyjroeder@gmail.com>
2022-01-01 00:14:10 -0700

    Test: distinct hometown list in the index page

--- spec/features/bachelorettes/contestants/index_spec.rb
+++ spec/features/bachelorettes/contestants/index_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'bachelorettes contestants index' do
 
   let!(:cont_1) { Contestant.create!(name: 'name_1', age: 1, hometown: 'ht_1', bachelorette: brette_1) }
   let!(:cont_2) { Contestant.create!(name: 'name_2', age: 2, hometown: 'ht_2', bachelorette: brette_1) }
+  let!(:cont_3) { Contestant.create!(name: 'name_3', age: 3, hometown: 'ht_2', bachelorette: brette_1) }
   
   before(:each) { visit bachelorette_contestants_path(brette_1) }
   describe 'view elements' do
@@ -24,6 +25,11 @@ RSpec.describe 'bachelorettes contestants index' do
     it 'displays contestants name as a link to the contestants show page' do
       expect(page).to have_link(cont_1.name, href: contestant_path(cont_1))
       expect(page).to have_link(cont_2.name, href: contestant_path(cont_2))
+    end
+
+    it 'displays unique list of contestant hometowns' do
+      save_and_open_page
+      expect(page).to have_content("These Contestants are from these hometowns: ht_1, ht_2")
     end
   end
 
