52597de6bf471a84f0744f0848a7458cf6636f67
Tim Roeder <timothyjroeder@gmail.com>
2021-12-31 23:37:21 -0700

    Test: add bachelorettes show page avg contestant age

--- spec/features/bachelorettes/show_spec.rb
+++ spec/features/bachelorettes/show_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe 'bachelorette show page', type: :feature do
   let!(:brette_1) { Bachelorette.create!(name: 'brette_1', season_number: 1, description: 'desc_1') }
 
+  let!(:cont_1) { Contestant.create!(name: 'name_1', age: 1, hometown: 'ht_1', bachelorette: brette_1) }
+  let!(:cont_2) { Contestant.create!(name: 'name_2', age: 2, hometown: 'ht_2', bachelorette: brette_1) }
+  let!(:cont_3) { Contestant.create!(name: 'name_2', age: 3, hometown: 'ht_3', bachelorette: brette_1) }
+
   before(:each) { visit bachelorette_path(brette_1) }
 
   describe 'view elements' do
@@ -14,6 +18,10 @@ RSpec.describe 'bachelorette show page', type: :feature do
 
     it 'displays a link to bachelorettes contestants' do
       expect(page).to have_link('Contestants', href: bachelorette_contestants_path(brette_1))
+    end
+
+    it 'displays the average age of all contestants' do
+      expect(page).to have_content("Average Age of Contestants: 2.0")
     end
   end
 
