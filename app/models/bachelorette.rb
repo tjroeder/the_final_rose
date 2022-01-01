48b1e6d2959c96b9e4d05a8bb1f912ef5ec6ba20
Tim Roeder <timothyjroeder@gmail.com>
2021-12-31 23:35:27 -0700

    Feat: #contestant_avg_age created

--- app/models/bachelorette.rb
+++ app/models/bachelorette.rb
@@ -2,4 +2,9 @@ class Bachelorette < ApplicationRecord
   has_many :contestants
 
   validates_presence_of :name, :season_number, :description
+
+  # Instance Methods
+  def contestant_avg_age
+    contestants.average(:age).round(1)
+  end
 end
